be858112f439134eeb24b0f46f17aaf1088b6a78
PrernaKumari12 <91779020+PrernaKumari12@users.noreply.github.com>
2021-10-02 14:18:03 +0530

    Add files via upload

---
+++ maze_blocked.java
@@ -0,0 +1,26 @@
+package lecture_29;
+
+public class maze_blocked {
+	public static void main(String[] args) {
+		int[][] maze1 = { { 0, 1, 0, 0 }, { 0, 0, 0, 0 }, { 0, 1, 0, 0 }, { 0, 0, 1, 0 } };
+		maze(maze1, 0, 0,new boolean[4][4] ,"" );
+	}
+
+	public static void maze(int[][] maze, int row, int col, boolean[][] visited, String ans) {
+		if (row == -1 || row == maze.length || col == -1 || col == maze[0].length ||  maze[row][col] == 1||visited[row][col]) {
+			return;
+		}
+		
+		if (row == maze.length - 1 && col == maze[0].length-1) {
+			System.out.println(ans);
+			return;
+		}
+		
+		maze(maze, row - 1, col, visited, ans + "U");
+		maze(maze, row + 1, col, visited, ans + "D");
+		maze(maze, row, col - 1, visited, ans + "L");
+		maze(maze, row, col + 1, visited, ans + "R");
+		visited[row][col] = false;
+	}
+
+}
